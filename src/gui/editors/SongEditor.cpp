f1fe087a8c23545b3a696f75db7657f29db1ed81
extern-stat <externstat@outlook.com>
2026-05-09 11:13:09 -0400

    Add triplets to song editor snap sizes

--- src/gui/editors/SongEditor.cpp
+++ src/gui/editors/SongEditor.cpp
@@ -68,7 +68,7 @@ constexpr int MIN_PIXELS_PER_BAR = 4;
 constexpr int MAX_PIXELS_PER_BAR = 400;
 constexpr int ZOOM_STEPS = 200;
 
-constexpr std::array SNAP_SIZES{8.f, 4.f, 2.f, 1.f, 1/2.f, 1/4.f, 1/8.f, 1/16.f};
+constexpr std::array SNAP_SIZES{8.f, 4.f, 2.f, 1.f, 1/2.f, 1/4.f, 1/8.f, 1/16.f, 1/3.f, 1/6.f, 1/12.f};
 constexpr std::array PROPORTIONAL_SNAP_SIZES{64.f, 32.f, 16.f, 8.f, 4.f, 2.f, 1.f, 1/2.f, 1/4.f, 1/8.f, 1/16.f, 1/32.f, 1/64.f};
 
 }
